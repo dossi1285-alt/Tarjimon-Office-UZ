128f50c0e2b8969b97ded2a690f175b2f5c00856
dossi1285-alt <dossi1285@gmail.com>
2026-08-12 15:34:08 +0500

    source: add translation direction

---
+++ TarjimonOfficeUZ.Core/Translation/TranslationDirection.cs
@@ -0,0 +1,11 @@
+﻿namespace TarjimonOfficeUZ.Core.Translation
+{
+    public enum TranslationDirection
+    {
+        None = 0,
+
+        LatinToCyrillic = 1,
+
+        CyrillicToLatin = 2
+    }
+}
