8194cb0c808b566dc7c0d5eb6951b04b6b7d40ef
dossi1285-alt <dossi1285@gmail.com>
2026-08-12 15:34:25 +0500

    source: add reverse translation cache

---
+++ TarjimonOfficeUZ.Core/Translation/ReverseTranslationCache.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TarjimonOfficeUZ.Core.Translation
+{
+    internal sealed class ReverseTranslationCache
+    {
+        private readonly Dictionary<string, string> _cache = new Dictionary<string, string>();
+
+        public bool TryGet(string key, out string value)
+        {
+            return _cache.TryGetValue(key, out value);
+        }
+
+        public void Set(string key, string value)
+        {
+            _cache[key] = value;
+        }
+    }
+}
