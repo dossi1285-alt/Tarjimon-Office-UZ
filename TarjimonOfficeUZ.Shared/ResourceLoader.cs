489d0d9da923bc3d451ec7b2a9402d886cd733e5
dossi1285-alt <dossi1285@gmail.com>
2026-08-12 16:27:05 +0500

    Move shared ResourceLoader into Shared project

---
+++ TarjimonOfficeUZ.Shared/ResourceLoader.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Drawing;
+using System.IO;
+using System.Reflection;
+
+namespace TarjimonOfficeUZ.Shared
+{
+    public static class ResourceLoader
+    {
+        private static readonly Assembly assembly = typeof(ResourceLoader).Assembly;
+        public static Bitmap Load(string resourceName)
+        {
+            Assembly assembly = typeof(ResourceLoader).Assembly;
+
+            using (Stream stream = assembly.GetManifestResourceStream(resourceName))
+            {
+                if (stream == null)
+                    throw new Exception("Resurs topilmadi: " + resourceName);
+
+                return new Bitmap(stream);
+            }
+        }
+
+        public static Bitmap A_A =>
+            Load("TarjimonOfficeUZ.Shared.Resources.Icons.A_A.png");
+
+        public static Bitmap Kalit =>
+            Load("TarjimonOfficeUZ.Shared.Resources.Icons.Kalit.png");
+    }
+}
