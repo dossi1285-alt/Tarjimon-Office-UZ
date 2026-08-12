2657b8fd36e72c5b72cc14b7a1505034d2e565fb
dossi1285-alt <dossi1285@gmail.com>
2026-08-12 15:35:27 +0500

    source: add settings model

---
+++ TarjimonOfficeUZ.Shared/Managers/SettingsModel.cs
@@ -0,0 +1,15 @@
+﻿namespace TarjimonOfficeUZ.Shared.Managers
+{
+    public class SettingsModel
+    {
+        public string Language { get; set; } = "uz";
+
+        public bool AutoCheckUpdates { get; set; } = true;
+
+        public bool StartWithWord { get; set; }
+
+        public bool StartWithExcel { get; set; }
+
+        public bool EnableFutureAlphabet { get; set; } = false;
+    }
+}
