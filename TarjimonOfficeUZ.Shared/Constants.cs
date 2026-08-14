d4c57cb80842ae241c70b0f5f6974f432aafc2fd
dossi1285-alt <dossi1285@gmail.com>
2026-08-14 16:12:17 +0500

    Set real project website, support and release URLs

--- TarjimonOfficeUZ.Shared/Constants.cs
+++ TarjimonOfficeUZ.Shared/Constants.cs
@@ -1,18 +1,13 @@
-﻿
-
 namespace TarjimonOfficeUZ.Shared
 {
-    
-        public static class Constants
-        {
+    public static class Constants
+    {
         public const string Company = "KL Office";
         public const string Product = "Tarjimon Office UZ";
         public const string Version = "1.0.0";
         public const string Copyright = "© KL Office";
-        public const string Website = "...";
-        public const string SupportEmail = "...";
-        public const string UpdateUrl = "...";
+        public const string Website = "https://github.com/dossi1285-alt/Tarjimon-Office-UZ";
+        public const string SupportEmail = "https://github.com/dossi1285-alt/Tarjimon-Office-UZ/issues";
+        public const string UpdateUrl = "https://github.com/dossi1285-alt/Tarjimon-Office-UZ/releases/latest";
     }
-    
 }
-
